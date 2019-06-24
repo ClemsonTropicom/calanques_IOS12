65477867b7730305b0e6769499b5602ba2529d9e
Clément Doche <webmaster.latropicom@gmail.com>
2019-06-24 16:15:47 -0400

    Ajout de la logique Delete/Reload avec bar button item

--- Les Calanques/Controller/ControllerAvecTableView.swift
+++ Les Calanques/Controller/ControllerAvecTableView.swift
@@ -35,16 +35,6 @@ class ControllerAvecTableView: UIViewController, UITableViewDataSource, UITableV
             vc.calanqueRecue = sender as? Calanque
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return calanques.count
@@ -62,5 +52,20 @@ class ControllerAvecTableView: UIViewController, UITableViewDataSource, UITableV
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            calanques.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    @IBAction func reloadAction(_ sender: Any) {
+        calanques = CalanqueCollection().all()
+        tableView.reloadData()
+    }
+    
+    
+    
 
 }
