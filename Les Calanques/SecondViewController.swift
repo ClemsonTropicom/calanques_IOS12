1ce7f371a6711e97975d06c1df6c1c2198b271a0
Clément Doche <webmaster.latropicom@gmail.com>
2019-06-24 13:23:29 -0400

    Initial Commit

---
+++ Les Calanques/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Les Calanques
+//
+//  Created by Clément Doche on 6/24/19.
+//  Copyright © 2019 La Tropicom. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
